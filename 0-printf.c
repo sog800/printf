286044f63c66ea863f453bee37f2831e5bec7429
Cyrus <daniellongwe800@gmail.com>
2022-10-19 05:17:46 -0700

    %s and %c

--- 0-printf.c
+++ 0-printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - printing strings and chars
  * @format: format
@@ -9,32 +8,38 @@
 int _printf(const char *format, ...)
 {
 	va_list vl;
-	int i, j;
+	int i = 0, j = 0;
 	char buff[100] = {0};
-	char *str_arg;
+	char *str_arg, c;
 
-	i = j = 0;
+	if (format == NULL)
+	{
+		exit(1);
+	}
 	va_start(vl, format);
 	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
+			if (format[i] == '%')
 			{
-				case 'c':
-					{
-						buff[j] = (char)va_arg(vl, int);
-						j++;
-						break;
-					}
-				case 's':
-					{
-						str_arg = va_arg(vl, char *);
-						strcpy(&buff[j], str_arg);
-						j += strlen(str_arg);
-						break;
-					}
+				c = '%';
+				write(1, &c , 1);
+			}
+			else
+			{
+				if (format[i] == 'c')
+				{
+					buff[j] = (char)va_arg(vl, int);
+					j++;
+				}
+				else if (format[i] == 's')
+				{
+					str_arg = va_arg(vl, char *);
+					strcpy(&buff[j], str_arg);
+					j += strlen(str_arg);
+				}
 			}
 		}
 		else
@@ -47,4 +52,13 @@ int _printf(const char *format, ...)
 	fwrite(buff, j, 1, stdout);
 	va_end(vl);
 	return (j);
+}
+int main(void)
+{
+	char *name;
+	char latter;
+	name = "Daniel";
+	latter = 'A';
+	_printf("my name is %s and my latter is %c\n", name, latter);
+	return (0);
 }
